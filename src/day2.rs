635fe7f78cda9e95ed99d24b9e2fbc28f21c28b1
Jovasa <jovasa46@gmail.com>
2022-07-20 22:50:49 +0300

    Day2 second

--- src/day2.rs
+++ src/day2.rs
@@ -5,21 +5,28 @@ use itertools::Itertools;
 fn main() {
     let reader = BufReader::new(File::open("data/day2.txt").unwrap());
 
-    let mut total = 0;
+    let mut paper_area = 0;
+    let mut ribbon_length = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
         let mut minimum = i32::MAX;
-        line
+        let dimensions: Vec<i32> = line
             .split("x")
-            .map(|x| x.parse().unwrap())
+            .map(|x| x.parse().unwrap()).sorted().collect();
+        dimensions
+            .iter()
             .combinations(2)
-            .for_each(|x: Vec<i32>| {
+            .for_each(|x: Vec<&i32>| {
                 let i = 2 * x[0] * x[1];
-                total += i;
+                paper_area += i;
                 minimum = minimum.min(i / 2);
             });
-        total += minimum;
+        paper_area += minimum;
+
+        ribbon_length += dimensions[0] * 2 + dimensions[1] * 2;
+        ribbon_length += dimensions.iter().fold(1, |acc, &x| acc * x);
     }
-    println!("{}", total);
+    println!("{}", paper_area);
+    println!("{}", ribbon_length);
 }
