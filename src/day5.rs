b5ffe59e598fb4850655e89b8a39425bc6157169
Jovasa <jovasa46@gmail.com>
2022-07-21 19:22:28 +0300

    Day5 second

--- src/day5.rs
+++ src/day5.rs
@@ -1,8 +1,14 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use itertools::Itertools;
 
 fn main() {
+    first();
+    second();
+}
 
+fn first() {
     let reader = BufReader::new(File::open("data/day5.txt").unwrap());
 
     let mut nice_strings = 0;
@@ -14,7 +20,7 @@ fn main() {
         let mut got_invalid = false;
         for c in line.unwrap().chars() {
             match c {
-                'a' | 'e' | 'i' | 'u' | 'o' =>  vowels += 1,
+                'a' | 'e' | 'i' | 'u' | 'o' => vowels += 1,
                 _ => {}
             }
             got_double |= previous == c;
@@ -30,6 +36,42 @@ fn main() {
         if vowels >= 3 && got_double && !got_invalid {
             nice_strings += 1;
         }
+    }
+    println!("{}", nice_strings);
+}
+
+fn second() {
+    let reader = BufReader::new(File::open("data/day5.txt").unwrap());
+
+    let mut nice_strings = 0;
+
+    for line in reader.lines() {
+        let mut first_rule = false;
+        let mut second_rule = false;
+        let mut pairs = HashSet::new();
+        let string = line.unwrap();
+        let vec = string.split("").collect_vec();
+        vec.clone().windows(3).for_each(
+            |x| second_rule |= x[0] == x[2]
+        );
+        let mut buffer = ('_', '_');
+        vec.windows(2).for_each(
+            |x| {
+                let first = match x[0].chars().next() {
+                    Some(a) => a,
+                    None => '_'
+                };
+                let second = match x[1].chars().next(){
+                    Some(a) => a,
+                    None => '_'
+                };
+                let t = (first, second);
+                first_rule |= pairs.contains(&t);
+                pairs.insert(buffer);
+                buffer = t;
+            }
+        );
+        if first_rule && second_rule {nice_strings += 1;}
     }
     println!("{}", nice_strings);
 }
